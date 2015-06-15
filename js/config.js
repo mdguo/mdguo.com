78afd1c1dafc718a9cd46998715c50c707f4657d
Michael Guo <mikeguo1992@gmail.com>
2015-06-15 16:43:44 -0400

    Finished routing, added API url

--- js/config.js
+++ js/config.js
@@ -1,1 +1,15 @@
-// some js
+window.addEventListener('load', function() {
+	setTimeout(function() {
+		window.addEventListener('popstate', function(event) {
+			updateUI()
+		})
+	}, 0)
+})
+
+function updateUI() {
+	var loc = window.location.pathname
+	React.render(React.createElement(App, {route: loc}), document.getElementById('container'))
+}
+
+// initial load
+updateUI()
--- js/home.js
+++ js/home.js
@@ -1,1 +1,39 @@
 /** @jsx React.DOM */
+var About = React.createClass({displayName: "About",
+	render: function () {
+    	return (
+    		React.createElement("h2", null, "About")
+    		)
+	}
+})
+
+var Home = React.createClass({displayName: "Home",
+  	render: function () {
+    	return (
+    		React.createElement("div", null, 
+    			React.createElement("h2", null, "Website under construction, check back soon!"), 
+    			React.createElement("a", {href: "/resume"}, " Check out my resume")
+    		)
+    		)
+	}
+})
+
+var App = React.createClass({displayName: "App",
+  	render: function () {
+    	var Child
+	    switch (this.props.route) {
+	    	case '/about': 
+	    		Child = About
+	    		break
+	    	default:      
+	    		Child = Home
+	    		// TODO: modify url
+	    }
+
+	    return (
+	    	React.createElement("div", null, 
+	    		React.createElement(Child, null)
+	    	)
+	    	)
+  	}
+})
