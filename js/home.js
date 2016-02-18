c20d7a8c5c63ba7e9781314818ab5a64ea86984d
Michael Guo <mdguo@uwaterloo.ca>
2016-02-17 22:42:59 -0500

    selector wheel WIP

--- js/home.js
+++ js/home.js
@@ -11,8 +11,19 @@ var Home = React.createClass({displayName: "Home",
   	render: function () {
     	return (
     		React.createElement("div", null, 
-    			React.createElement("h2", null, "Website under construction, check back soon!"), 
-    			React.createElement("a", {href: "/resume"}, " Check out my resume")
+    			React.createElement("div", {className: "side-panel fluid"}, 
+    				React.createElement("h1", {className: "color-4"}, "Michael Guo"), 
+    				React.createElement("p", null, "Michael Guo is an aspiring web and software developer")
+    			), 
+    			React.createElement("div", {className: "main-panel fluid"}, 
+	    			React.createElement("div", {className: "selector-wheel relative"}, 
+	    				React.createElement("ul", {className: "center pie"}, 
+	    					React.createElement("li", {className: "slice slice-1"}, React.createElement("div", {className: "slice-contents"}, React.createElement("img", {src: "/images/Linkedin-icon.png"}))), 
+	    					React.createElement("li", {className: "slice slice-2"}, React.createElement("div", {className: "slice-contents"}, "github")), 
+	    					React.createElement("li", {className: "slice slice-3"}, React.createElement("div", {className: "slice-contents"}, "resume"))
+	    				)
+	    			)
+    			)
     		)
     		)
 	}
